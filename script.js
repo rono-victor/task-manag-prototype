dec3347774c84b70f65bc91622e583809aa4cc21
vvronc <ronovictor40@gmail.com>
2023-01-27 14:08:45 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector('.hamburger');
+const navlink = document.querySelector('.nav__link');
+
+hamburger.addEventListener('click',() => {
+    navlink.classList.toggle('hide');
+});
